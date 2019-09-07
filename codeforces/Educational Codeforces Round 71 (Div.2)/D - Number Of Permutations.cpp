93bf8fc889a7b184c7ffa1d7382bd4824fb380b4
oridya <38060133+orihehe@users.noreply.github.com>
2019-09-07 22:03:06 +0900

    19/09/07

---
+++ codeforces/Educational Codeforces Round 71 (Div.2)/D - Number Of Permutations.cpp
@@ -0,0 +1,52 @@
+﻿#include <cstdio>
+#include <algorithm>
+#include <map>
+#define ll long long
+#define pii pair<int,int>
+#define mod 998244353
+using namespace std;
+
+/* 🐣🐥 */
+ll calc(int n) {
+	ll ret = 1;
+	while (n > 1) {
+		ret *= n;
+		n--;
+		ret %= mod;
+	}
+	return ret;
+}
+int l[300001], r[300001];
+map<pii, int> mp;
+pii arr[300001];
+int main() {
+	ll ans, mn1 = 1, mn2 = 1, mn3 = 1;
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d %d", &arr[i].first, &arr[i].second);
+		l[arr[i].first]++, r[arr[i].second]++;
+		mp[arr[i]]++;
+	}
+	sort(arr, arr + n);
+	bool flag = true;
+	for (int i = 1; i < n; i++) {
+		if (arr[i].second < arr[i - 1].second) flag = false;
+	}
+	ans = calc(n);
+	for (int i = 1; i <= n; i++) {
+		mn1 *= calc(l[i]);
+		mn1 %= mod;
+	}
+	for (int i = 1; i <= n; i++) {
+		mn2 *= calc(r[i]);
+		mn2 %= mod;
+	}
+	for (auto iter = mp.begin(); iter != mp.end(); iter++) {
+		mn3 *= calc(iter->second);
+		mn3 %= mod;
+	}
+	printf("%lld", (ans - mn1 - mn2 + (flag ? mn3 : 0) + mod * 2LL) % mod);
+
+	return 0;
+}
---
+++ codeforces/Educational Codeforces Round 71 (Div.2)/E - XOR Guessing.cpp
@@ -0,0 +1,32 @@
+﻿#include <cstdio>
+using namespace std;
+
+/* 🐣🐥 */
+int main() {
+	int x, res, ans = 0;
+	printf("? "); x = 127;
+	for (int i = 0; i < 100; i++) {
+		printf("%d ", x);
+		x += 128;
+	}
+	printf("\n");
+	fflush(stdout);
+	scanf("%d", &res);
+	for (int i = 0; i < 7; i++) {
+		if ((res | (1 << i)) > res) ans += (1 << i);
+	}
+	printf("? "); x = 16256;
+	for (int i = 0; i < 100; i++) {
+		printf("%d ", x++);
+	}
+	printf("\n");
+	fflush(stdout);
+	scanf("%d", &res);
+	for (int i = 7; i < 14; i++) {
+		if ((res | (1 << i)) > res) ans += (1 << i);
+	}
+	printf("! %d\n", ans);
+	fflush(stdout);
+
+	return 0;
+}
